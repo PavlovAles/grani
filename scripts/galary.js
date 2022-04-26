db8822e2cea4bfdd1f8f6cf9acbd5fc745a11959
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-04-26 12:32:39 +0300

    feat: скрипт копирования изображений из галереи в попап

--- scripts/galary.js
+++ scripts/galary.js
@@ -1,28 +1,46 @@
 const gallaryCollage = document.querySelector(".galary__collage");
-const collageItems = Array.from(
-  gallaryCollage.querySelectorAll(".galary__collage-item")
-);
+const collageImgs = Array.from(gallaryCollage.querySelectorAll(".galary__img"));
 const popup = document.querySelector(".galary-popup");
-const imgContainer = popup.querySelector(".galary-popup__img-container");
+const popupImgContainer = popup.querySelector(".galary-popup__img-container");
 const btnClose = popup.querySelector(".galary-popup__btn_type_close");
 const btnNext = popup.querySelector(".galary-popup__btn_type_next");
 const btnPrev = popup.querySelector(".galary-popup__btn_type_prev");
 
 function openPopup(evt) {
+  markActiveImg(evt.target);
   popup.classList.add("galary-popup_opened");
   document.body.style.overflow = "hidden";
+}
+
+function markActiveImg(img) {
+  img.classList.add("galary__img_clicked");
+  activeIndex = collageImgs.findIndex((img) =>
+    img.classList.contains("galary__img_clicked")
+  );
+  popupImgs[activeIndex].classList.add("galary-popup__big-img_active");
 }
 
 function closePopup(evt) {
   popup.classList.remove("galary-popup_opened");
   document.body.style.overflow = "visible";
+  unmarkActiveImg();
+  popupImgs.forEach((img) => clearImgClassList(img));
 }
 
-function moveLeft() {
-  let activeImg = imgContainer.querySelector(".galary-popup__big-img_active");
+function unmarkActiveImg() {
+  collageImgs.forEach((img) => img.classList.remove("galary__img_clicked"));
+  popupImgs.forEach((img) =>
+    img.classList.remove("galary-popup__big-img_active")
+  );
+}
+
+function showNext() {
+  let activeImg = popupImgContainer.querySelector(
+    ".galary-popup__big-img_active"
+  );
   clearImgClassList(activeImg);
   const nextImg =
-    activeImg.nextElementSibling ?? imgContainer.firstElementChild;
+    activeImg.nextElementSibling ?? popupImgContainer.firstElementChild;
   clearImgClassList(nextImg);
   activeImg.classList.add("galary-popup__big-img_move-left");
   activeImg.classList.remove("galary-popup__big-img_active");
@@ -32,11 +50,13 @@ function moveLeft() {
   );
 }
 
-function moveRight() {
-  let activeImg = imgContainer.querySelector(".galary-popup__big-img_active");
+function showPrev() {
+  let activeImg = popupImgContainer.querySelector(
+    ".galary-popup__big-img_active"
+  );
   clearImgClassList(activeImg);
   const nextImg =
-    activeImg.previousElementSibling ?? imgContainer.lastElementChild;
+    activeImg.previousElementSibling ?? popupImgContainer.lastElementChild;
   clearImgClassList(nextImg);
   activeImg.classList.add("galary-popup__big-img_move-right");
   activeImg.classList.remove("galary-popup__big-img_active");
@@ -46,8 +66,8 @@ function moveRight() {
   );
 }
 
-function clearImgClassList(imgNode) {
-  imgNode.classList.remove(
+function clearImgClassList(img) {
+  img.classList.remove(
     "galary-popup__big-img_move-right",
     "galary-popup__big-img_move-left",
     "galary-popup__big-img_appears-from-right",
@@ -55,10 +75,22 @@ function clearImgClassList(imgNode) {
   );
 }
 
-gallaryCollage.addEventListener("click", openPopup);
-
 btnClose.addEventListener("click", closePopup);
 
-btnNext.addEventListener("click", moveLeft);
+btnNext.addEventListener("click", showNext);
 
-btnPrev.addEventListener("click", moveRight);
+btnPrev.addEventListener("click", showPrev);
+
+collageImgs.forEach((img) => {
+  img.addEventListener("click", openPopup);
+  let popupImg = img.cloneNode(true);
+  popupImg.classList.remove("galary__img");
+  popupImg.classList.add("galary-popup__big-img");
+  popupImgContainer.appendChild(popupImg);
+});
+
+const popupImgs = Array.from(
+  popupImgContainer.querySelectorAll(".galary-popup__big-img")
+);
+
+popupImgs.forEach((img) => img.addEventListener("click", closePopup));
